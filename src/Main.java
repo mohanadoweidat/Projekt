5646746809dabafd60f22e6e430f39d98120cbea
mohanad oweidat <mohanad.owidat@hotmail.com>
2021-02-24 11:36:28 +0100

    Test3

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main
 {
     public static void main(String[] args) {
         System.out.println("Test from sara");
+        System.out.println("hejsaraaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaah //Susanne");
+        System.out.println("Mohanad newwww");
     }
 }
+
